37ba033d32aa109e3a17cce524cca19c10d79f18
Marcin Kowal <marcink@medius-group.com>
2017-06-04 21:30:49 +0200

    Page reorganized

--- Frontend/server.js
+++ Frontend/server.js
@@ -11,8 +11,8 @@ app.get('/ES6Playground',(req, res) => {
     res.sendFile(path.join(__dirname + '/ES6Playground/index.html'));
 });
 
-app.use(express.static(path.join(__dirname + '/Portfolio/assets')));
+app.use('/Portfolio/assets', express.static(path.join(__dirname + '/Portfolio/assets')));
 
-app.use(express.static(path.join(__dirname + '/ES6Playground/assets')));
+app.use('/ES6Playground/assets', express.static(path.join(__dirname + '/ES6Playground/assets')));
 
 app.listen(3000);
