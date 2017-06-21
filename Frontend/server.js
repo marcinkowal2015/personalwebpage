76715a235e1a4f8f08fb6a5abc4d9847da126c1b
marcinkowal2015 <marcinkowal2015@outlook.com>
2017-06-21 22:39:35 +0200

    New feature

--- Frontend/server.js
+++ Frontend/server.js
@@ -11,8 +11,14 @@ app.get('/ES6Playground',(req, res) => {
     res.sendFile(path.join(__dirname + '/ES6Playground/index.html'));
 });
 
+app.get('/rz', (req, res) => {
+    res.sendFile(path.join(__dirname + '/FunnyLetters/index.html'));
+});
+
 app.use('/Portfolio/assets', express.static(path.join(__dirname + '/Portfolio/assets')));
 
 app.use('/ES6Playground/assets', express.static(path.join(__dirname + '/ES6Playground/assets')));
+
+app.use('/FunnyLetters/assets', express.static(path.join(__dirname + '/FunnyLetters/assets')));
 
 app.listen(3000);
