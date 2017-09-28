1cb1d259267e42cc95f48eec2789467410bb969d
marcinkowal2015 <marcinkowal2015@outlook.com>
2017-09-28 21:47:47 +0200

    fallback to the old page plus new project added

--- Frontend/server.js
+++ Frontend/server.js
@@ -4,10 +4,6 @@ const path = require("path");
 const app = express();
 
 app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname + '/Portfolio-old/index.html'));
-});
-
-app.get('/new',(req, res) => {
     res.sendFile(path.join(__dirname + '/Portfolio/index.html'));
 });
 
@@ -19,12 +15,16 @@ app.get('/rz', (req, res) => {
     res.sendFile(path.join(__dirname + '/FunnyLetters/index.html'));
 });
 
-app.use('/Portfolio/assets', express.static(path.join(__dirname + '/Portfolio/assets')));
+app.get('/achievements', (req, res) => {
+    res.sendFile(path.join(__dirname + '/Achievements/index.html'));
+});
 
-app.use('/Portfolio-old/assets', express.static(path.join(__dirname + '/Portfolio-old/assets')));
+app.use('/Portfolio/assets', express.static(path.join(__dirname + '/Portfolio/assets')));
 
 app.use('/ES6Playground/assets', express.static(path.join(__dirname + '/ES6Playground/assets')));
 
 app.use('/FunnyLetters/assets', express.static(path.join(__dirname + '/FunnyLetters/assets')));
+
+app.use('/Achievements/assets', express.static(path.join(__dirname + '/Achievements/assets')));
 
 app.listen(3000);
